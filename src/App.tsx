e0cc116d4184a471c98fadd151ce7eb0ddaa976c
Benjamin <benjaminmkellner@gmail.com>
2024-02-15 19:20:40 -0500

    Update function to handle edge cases

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Benjamin Kellner is the greatest Hello World of all time CISC275
+                Benjamin Kellner is the greatest Hello World of all time CISC
             </header>
+            <header className="App-header">This is a jack richter page</header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
