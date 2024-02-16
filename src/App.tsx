df7791573acf8b6372dbb44221e4079ee8f87df6
Benjamin <benjaminmkellner@gmail.com>
2024-02-16 13:30:26 -0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import picture from "/Users/ben/tasks/src/Images/the_blimp.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <header className="App-header">
+                Benjamin Kellner is the greatest Hello World CISC275
+            </header>
             <header className="App-header">
                 Ceeve is the biggest blimp in the entire world
             </header>
@@ -20,6 +24,37 @@ function App(): JSX.Element {
                 <li>He sounds like a blimp</li>
                 <li>He acts like a blimp</li>
             </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Click If Blimp
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        First Column.
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "#eb343d"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Second Column.
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "#eb343d"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload. Hello World!
+            </p>
         </div>
     );
 }
