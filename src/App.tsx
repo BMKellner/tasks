2671cba51d082c3389d2e38188b67d4a4e73dca4
Benjamin <benjaminmkellner@gmail.com>
2024-02-15 20:46:23 -0500

    revert back to old version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import logo from "./the_blimp.jpg";
 
 function App(): JSX.Element {
     return (
@@ -8,8 +7,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 Benjamin Kellner is the greatest Hello World of all time CISC275
             </header>
-            <header className="App-header">This is a Schmeven page</header>
-            <img src={logo} alt="The Blimp Cerca 2019" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
